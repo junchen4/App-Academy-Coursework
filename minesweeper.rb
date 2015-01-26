732c0f82edb0d5312784de40de23acbfa61a9afc
Jun + Scott <Jun + Scott@appacademy.io>
2015-01-26 11:34:35 -0500

    Create board, tile class. Create starting board and positions

---
+++ minesweeper.rb
@@ -0,0 +1,41 @@
+class Board
+
+  def self.blank_grid
+    Array.new(9) { Array.new(9) }
+  end
+
+  def initialize(rows = self.class.blank_grid)
+    @rows = rows
+  end
+
+  def [](pos)
+    x, y = pos[0], pos[1]
+    @rows[x][y]
+  end
+
+  # def []=(pos, mark)
+  #   raise "mark already placed there!" unless empty?(pos)
+  #
+  #   x, y = pos[0], pos[1]
+  #   @rows[x][y] = mark
+  # end
+
+
+end
+
+
+
+
+
+class Tile
+
+  def initialize(revealed = false, bomb, flagged = false)
+    @revealed = revealed?
+    @bomb =
+    @flagged =
+
+  end
+
+
+
+end
