9350e025484651e534222c25f6ba8c2847d95369
JoeandJun <JoeandJun@aa.io>
2015-02-11 11:22:16 -0500

    Add password methods to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   validates :user_name, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :user_name, uniqueness: true
   after_initialize :ensure_session_token
 
   def self.generate_session_token
@@ -10,6 +12,29 @@ class User < ActiveRecord::Base
     self.session_token = self.class.generate_session_token
     self.save!
     self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password
+    @password
+  end
+
+  def is_password?(password)
+    password_digest.is_password?(password)
+  end
+
+  def password_digest
+    BCrypt::Password.new(super)
+  end
+
+  def self.find_by_creditials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
   end
 
   private
